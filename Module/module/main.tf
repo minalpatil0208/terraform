55f56640fdf5dfe2f7441ec31ab4720a8eef427f
minal <minalpatil0208@gmail.com>
2026-02-15 17:30:39 +0000

    new

--- Module/module/main.tf
+++ Module/module/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "/terraform/Module/main/EC2"
+  source = "home/minal/terraform/Module/main/EC2"
   webserver_ami = ami-0317b0f0a0144b137
   webserver_instance_type = t3.small
   webserver_key_name = swami
@@ -10,7 +10,7 @@ module "ec2" {
 } 
 
 module "vpc" {
-  source = "/terraform/Module/main/VPC"
+  source = "home/minal/terraform/Module/main/VPC"
   vpc_cidr = "172.30.0.0/16"
   vpc_subnetA_cidr = "172.30.128.0/20"
   public_ip = true
