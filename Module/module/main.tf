6c755e55a4b0d353fce6601168cfe9d529326a49
minal <minalpatil0208@gmail.com>
2026-02-15 19:42:53 +0000

    new

--- Module/module/main.tf
+++ Module/module/main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
   source = "/home/minal/terraform/Module/main/EC2"
-  webserver_ami = ami-0317b0f0a0144b137
-  webserver_instance_type = t3.small
-  webserver_key_name = swami
+  webserver_ami = "ami-0317b0f0a0144b137"
+  webserver_instance_type = "t3.small"
+  webserver_key_name = "swami"
   webserver_vpc_security_group_ids = module.vpc.webserversg
   webserver_disable_api_termination = true
   webserver_subnet_id = module.vpc.subnet_id
